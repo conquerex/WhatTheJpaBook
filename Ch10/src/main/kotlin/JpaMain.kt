027d0cf762fc3ba9707335c2fc723dee1f3309f0
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2021-11-03 10:17:42 +0900

    ch10

--- Ch10/src/main/kotlin/JpaMain.kt
+++ Ch10/src/main/kotlin/JpaMain.kt
@@ -1,23 +1,50 @@
 import javax.persistence.EntityManager
 import javax.persistence.Persistence
+import javax.persistence.Tuple
+import javax.persistence.TypedQuery
+import javax.persistence.criteria.*
+
 
 fun main(args: Array<String>) {
     //엔티티 매니저 팩토리 생성
     val emf = Persistence.createEntityManagerFactory("jpabook")
     val em = emf.createEntityManager() //엔티티 매니저 생성
 
-    val tx = em.transaction //트랜잭션 기능 획득
+    // 예제
+    val cb: CriteriaBuilder = em.criteriaBuilder
+    val cq: CriteriaQuery<Member> = cb.createQuery(Member::class.java)
+    val m: Root<Member> = cq.from(Member::class.java)
+    cq.select(m)
+    val query: TypedQuery<Member> = em.createQuery(cq)
+    val members: List<Member> = query.resultList
 
-    try {
-        tx.begin() //트랜잭션 시작
-        logic(em) //비즈니스 로직
-        tx.commit() //트랜잭션 커밋
-    } catch (e: Exception) {
-        e.printStackTrace()
-        tx.rollback() //트랜잭션 롤백
-    } finally {
-        em.close() //엔티티 매니저 종료
+    members.forEach {
+        println(">>> " + it.age)
     }
+
+    // 예제
+    val usernameEqual: Predicate = cb.equal(m.get<String>("username"), "샘플 ddd")
+    val ageDesc: Order = cb.desc(m.get<Int>("age"))
+    cq.select(m)
+        .where(usernameEqual)
+        .orderBy(ageDesc)
+    em.createQuery(cq).resultList.forEach {
+        println(">>> age : " + it.age)
+    }
+
+    // 예제
+    val cb2: CriteriaBuilder = em.criteriaBuilder
+    val cq2: CriteriaQuery<Object> = cb2.createQuery(Object::class.java)
+    val cq2_1: CriteriaQuery<Tuple> = cb2.createQuery(Tuple::class.java)
+    val list: List<Object> = em.createQuery(cq2).resultList
+    println(">>>> size : ${list.size}")
+
+    // 예제
+    cq.select(m).distinct(true)
+//    cq.multiselect(m.get<String>("username", m.get<Int>("age")))
+//    cq.select(cb.array(m.get<String>("username", m.get<Int>("age"))))
+
+
 
     emf.close() //엔티티 매니저 팩토리 종료
 }
