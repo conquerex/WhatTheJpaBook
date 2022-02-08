509f3cfdde7c86a0684a7186f560378f33c58123
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2022-02-08 16:06:29 +0900

    async를 테스트에서 사용해보자

---
+++ Sample/src/main/kotlin/what/the/jpabook/sample/AsyncConfig.kt
@@ -0,0 +1,47 @@
+package what.the.jpabook.sample
+
+import org.slf4j.MDC
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.TaskDecorator
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+
+@Configuration
+@EnableAsync
+class AsyncConfig(
+    @Value("\${sfn.async.poolSize:10}")
+    private val poolSize: Int,
+) {
+    @Bean
+    fun taskExecutor(): ThreadPoolTaskExecutor {
+        val taskExecutor = ThreadPoolTaskExecutor()
+
+        taskExecutor.setThreadNamePrefix("QueueTask-")
+        taskExecutor.corePoolSize = poolSize
+        taskExecutor.maxPoolSize = poolSize * 2
+        taskExecutor.setQueueCapacity(poolSize * 5)
+        taskExecutor.setTaskDecorator(LoggingTaskDecorator())
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true)
+
+        return taskExecutor
+    }
+}
+
+/**
+ * 각 비동기 쓰레드마다 로그를 남길 경우 자신을 호출한 쓰레드를 알 수 있어, 디버그시 추적이 용이해진다.
+ */
+class LoggingTaskDecorator : TaskDecorator {
+
+    override fun decorate(task: Runnable): Runnable {
+        val callerThreadContext = MDC.getCopyOfContextMap()
+
+        return Runnable {
+            callerThreadContext?.let {
+                MDC.setContextMap(it)
+            }
+            task.run()
+        }
+    }
+}
---
+++ Sample/src/test/kotlin/what/the/jpabook/sample/SampleApplicationTests.kt
@@ -0,0 +1,67 @@
+package what.the.jpabook.sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+import java.time.LocalTime
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+
+@SpringBootTest
+class SampleApplicationTests {
+
+    @Autowired
+    lateinit var a: Sample
+
+    @Test
+    fun test() {
+        println("Before, in test: " + Thread.currentThread())
+        for (i in 0..5) {
+            a.async()
+        }
+
+        for (i in 0..5) {
+            a.sync()
+        }
+        println("After, in test: " + Thread.currentThread())
+    }
+
+    @Test
+    fun test2() {
+        println(LocalTime.now().toString() + " Start >>>")
+        val executor: ExecutorService = Executors.newSingleThreadExecutor()
+
+        val future: Future<Int> = executor.submit<Int> {
+            println(LocalTime.now().toString() + " Starting runnable")
+            val sum = 1 + 1
+            Thread.sleep(3000)
+            sum
+        }
+
+        println(LocalTime.now().toString() + " Waiting the task done")
+        val result: Int = future.get()
+        println(LocalTime.now().toString() + " Result : " + result)
+    }
+
+}
+
+@Component
+class Sample {
+    @Async("taskExecutor")
+    fun async() {
+        Thread.sleep(100)
+        println("""
+            In async: ${Thread.currentThread()} // ${LocalTime.now()}
+        """.trimIndent())
+    }
+
+    fun sync() {
+        Thread.sleep(100)
+        println("""
+            In sync: ${Thread.currentThread()} // ${LocalTime.now()}
+        """.trimIndent())
+    }
+}
